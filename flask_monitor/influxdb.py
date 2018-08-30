d369ed78398ee778674bd277a7886cdc6aa09fc1
dcy <timedcy@gmail.com>
2018-08-30 10:51:33 +0800

    Update influxdb.py
    添加过滤忽略的path功能

--- flask_monitor/influxdb.py
+++ flask_monitor/influxdb.py
@@ -8,7 +8,7 @@ from influxdb.client import InfluxDBClientError
 
 class ObserverInfluxdb(ObserverMetrics):
 
-    def __init__(self, host, port, user, password, db, ssl=False, verify_ssl=False, measure='flask', *args, **kw):
+    def __init__(self, host, port, user, password, db, ssl=False, verify_ssl=False, measure='flask', skip_path=None, *args, **kw):
         ObserverMetrics.__init__(self, *args, **kw)
         self._data = [
             {
@@ -18,6 +18,7 @@ class ObserverInfluxdb(ObserverMetrics):
             }
         ]
         self.field_set = set(['remote_addr', 'delta', 'start', 'asctime'])
+        self.skip_path = set(skip_path if skip_path else [])
         try:
             self.db = InfluxDBClient(host=host,
                                      port=port,
@@ -33,6 +34,8 @@ class ObserverInfluxdb(ObserverMetrics):
     def action(self, event):
         try:
             data = self._data
+            if event.dict and event.dict.get('path') in skip_path:
+                return
             data[0]['tags'] = {k: v for k, v in event.dict.items() if k not in self.field_set}
             data[0]['fields'] = {k: v for k, v in event.dict.items() if k in self.field_set}
             data[0]['fields']["cost"] = event.timing*1000.0
